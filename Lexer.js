18a95f620e9e62e6402dd93590da8c7127c2701a
Gary50613 <50613gary@gmail.com>
2021-05-23 09:04:17 +0800

    finished Position to show where has error

--- Lexer.js
+++ Lexer.js
@@ -1,39 +1,81 @@
 const Token = require("./Token")
+const Position = require("./Position")
 
 class Lexer {
-    static parse(input) {
-        return this._parse(input.trim(), [], 0)
+    constructor(input) {
+        this.position = new Position()
+        this.tokens = []
+        this.input = input
     }
 
-    static _parse(input, tokens, index) {
-        if(input.substr(index, 1).length === 0)
-            return tokens
+    static parse(input) {
+        return new Lexer(input.trim())._parse()
+    }
 
-        if(Token.DIGITS.includes(input.substr(index, 1))) {
-            let _num = '', _i = index
-            while(input.substr(_i, 1).length > 0 && (Token.DIGITS.includes(input.substr(_i, 1)) || input.substr(_i, 1) === '.')) {
-                _num += input.charAt(_i)
-                _i++
+    _parse() {
+        let char = this.input.substr(this.position.index, 1)
+
+        if(char.length === 0)
+            return this.tokens
+
+        if(Token.DIGITS.includes(this.input.substr(this.position.index, 1))) {
+            let _num = ''
+            while(char.length > 0 && (Token.DIGITS.includes(char) || char === '.')) {
+                _num += this.input.charAt(this.position.index)
+                this.position.nextChar()
+                char = this.input.substr(this.position.index, 1)
             }
 
-            if(_num.includes('.'))
-                return this._parse(input, [...tokens, new Token(Token.FLOAT, _num)], _i)
-            return this._parse(input, [...tokens, new Token(Token.INT, _num)], _i)
+            if(_num.includes('.')) {
+                this.tokens.push(new Token(Token.FLOAT, _num))
+                return this._parse()
+            }
+            this.tokens.push(new Token(Token.INT, _num))
+            return this._parse()
         }
 
-        if(input.substr(index, 1) === '+')
-            return this._parse(input, [...tokens, new Token(Token.PLUS)], index + 1)
-        if(input.substr(index, 1) === '-')
-            return this._parse(input, [...tokens, new Token(Token.MINUS)], index + 1)
-        if(input.substr(index, 1) === '*')
-            return this._parse(input, [...tokens, new Token(Token.MUL)], index + 1)
-        if(input.substr(index, 1) === '/')
-            return this._parse(input, [...tokens, new Token(Token.DIV)], index + 1)
-        if(input.substr(index, 1) === '(')
-            return this._parse(input, [...tokens, new Token(Token.LPAREN)], index + 1)
-        if(input.substr(index, 1) === ')')
-            return this._parse(input, [...tokens, new Token(Token.RPAREN)], index + 1)
-        return this._parse(input, [...tokens, new Token(Token.UNKNOWN)], index + 1)
+        if(char === ' ') {
+            this.position.nextChar()
+            return this._parse()
+        }
+
+        if(char === '\n') {
+            this.position.nextLine()
+            return this._parse()
+        }
+
+        if(char === '+') {
+            this.tokens.push(new Token(Token.PLUS))
+            this.position.nextChar()
+            return this._parse()
+        }
+        if(char === '-') {
+            this.tokens.push(new Token(Token.MINUS))
+            this.position.nextChar()
+            return this._parse()
+        }
+        if(char === '*') {
+            this.tokens.push(new Token(Token.MINUS))
+            this.position.nextChar()
+            return this._parse()
+        }
+        if(char === '/') {
+            this.tokens.push(new Token(Token.DIV))
+            this.position.nextChar()
+            return this._parse()
+        }
+        if(char === '(') {
+            this.tokens.push(new Token(Token.LPAREN))
+            this.position.nextChar()
+            return this._parse()
+        }
+        if(char === ')') {
+            this.tokens.push(new Token(Token.RPAREN))
+            this.position.nextChar()
+            return this._parse()
+        }
+
+        //TODO throw error
     }
 }
 
--- Position.js
+++ Position.js
@@ -1,3 +1,20 @@
 class Position {
+    constructor() {
+        this.col = 1
+        this.row = 1
+        this.index = 0
+    }
 
+    nextLine() {
+        this.col = 1
+        this.row++
+        this.index++
+    }
+
+    nextChar(length = 1) {
+        this.col += length
+        this.index += length
+    }
 }
+
+module.exports = Position
